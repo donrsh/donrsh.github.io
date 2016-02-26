945ebfbab9b96da6b7a6d1c0b8d40ef97840c751
don.shieh@gmail.com <don@DondeiMac.local>
2016-02-27 02:03:59 +0800

    layout ver2

--- build/bundle.js
+++ build/bundle.js
@@ -50,25 +50,6 @@
 
 	$(document).ready(function () {
 
-		//remove 'start' classname to initialze the animation
-		(function () {
-			var counter = 1;
-			$('.infoBlock.start').each(function () {
-				var self = this;
-				setTimeout(function () {
-					$(self).removeClass('start');
-				}, counter * 500);
-
-				counter++;
-			});
-		})();
-
-		//bind event on .itemTitle
-		$('.itemTitle').on('click', function () {
-			console.log('t!');
-			$(this).siblings('.itemContent').toggleClass('show');
-		});
-
 		$('footer').html('( Don Shieh &copy; ' + new Date().getFullYear() + ' )');
 	});
 
--- index.jsx
+++ index.jsx
@@ -3,6 +3,6 @@ var $ = require('jquery');
 
 $(document).ready(function(){
 
-	$('footer').html('( Don Shieh &copy; ' + new Date().getFullYear() + ')');
+	$('footer').html('( Don Shieh &copy; ' + new Date().getFullYear() + ' )');
 
 });
