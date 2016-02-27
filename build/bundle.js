2e023c382dcf027ab7310adb6f3014be3ca3ed45
don.shieh@gmail.com <don@DondeiMac.local>
2016-02-27 16:13:49 +0800

    add navbar animation

--- build/bundle.js
+++ build/bundle.js
@@ -51,6 +51,33 @@
 	$(document).ready(function () {
 
 		$('footer').html('( Don Shieh &copy; ' + new Date().getFullYear() + ' )');
+
+		setTimeout(function () {
+			$('#cssskills').removeClass('start');
+		}, 500);
+
+		setTimeout(function () {
+			$('#works').removeClass('start');
+		}, 1000);
+
+		setTimeout(function () {
+			$('#jsskills').removeClass('start');
+		}, 1500);
+
+		setTimeout(function () {
+			$('#worksurroundings').removeClass('start');
+		}, 2000);
+
+		$(window).on('scroll', function () {
+			// console.log(window.pageYOffset);
+			if (window.pageYOffset > 0) {
+				$('#selfpart').addClass('selfpart-nav');
+			}
+
+			if (window.pageYOffset == 0) {
+				$('#selfpart').removeClass('selfpart-nav');
+			}
+		});
 	});
 
 /***/ },
