89d9d5db686eb0f290745fa11c9518e2eeb2d519
don.shieh@gmail.com <don@DondeiMac.local>
2016-02-28 22:03:29 +0800

    add body padding-top when scrolling

--- build/bundle.js
+++ build/bundle.js
@@ -69,14 +69,14 @@
 		}, 2000);
 
 		$(window).on('scroll', function () {
-			console.log(window.pageYOffset);
+			// console.log(window.pageYOffset);
 			if (window.pageYOffset > 0) {
 				$('#selfpart').addClass('selfpart-nav');
 				$('body').addClass('fatPadding');
 			}
 
 			if (window.pageYOffset <= 0) {
-				console.log('alert!');
+				// console.log('alert!')
 				$('#selfpart').removeClass('selfpart-nav');
 				$('body').removeClass('fatPadding');
 			}
